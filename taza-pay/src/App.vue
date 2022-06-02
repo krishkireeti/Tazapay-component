2f8c2ac2f45d934138a5a6f991399669e894d06a
unknown <kirikom94@gmail.com>
2022-06-02 21:26:01 +0530

    first commit

---
+++ taza-pay/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Please select the values from the dropdown</h1>
+    <DropDown />
+  </div>
+</template>
+
+<script>
+
+import DropDown from './components/DropDown.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DropDown
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ taza-pay/src/components/DropDown.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="dropdown">
+   <v-select
+    v-model="dropdown_langs_ids"
+    :options=" dropdown_langs"
+    label="name"
+    multiple
+    placeholder="Select"
+    class="payment_methods__select2"
+    :reduce="(dropdown_lang) => dropdown_lang.id">
+   </v-select>
+  </div>
+</template>
+
+<script>
+import vSelect from "vue-select";
+import "vue-select/dist/vue-select.css";
+export default {
+  name: "DropDown",
+  components: {
+    vSelect,
+  },
+
+  data() {
+    return {
+      dropdown_langs: [
+        {
+          id: 1,
+          name: "Javascript",
+        },
+        {
+          id: 2,
+          name: "C++",
+        },
+        {
+          id: 3,
+          name: "Python",
+        },
+        {
+          id: 4,
+          name: "Ruby",
+        },
+        {
+          id: 5,
+          name: "PHP",
+        },
+        {
+          id: 6,
+          name: "Pascal",
+        },
+      ],
+      dropdown_langs_ids: [],
+    };
+  },
+};
+</script>
